2b52b94c9a0397b9b4c91d4e32b325b826d43610
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-16 01:11:13 +0530

    fixed all register controller bugs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,10 +32,16 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  console.log(avatarLocalPath);
-  console.log(coverImageLocalPath);
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage.path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is Required !");
