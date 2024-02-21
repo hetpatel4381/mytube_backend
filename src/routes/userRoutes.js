e0446346454234eaf540a457b6c279cfb136503d
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-21 15:23:15 +0530

    added new routes for userController

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/userController.js";
 import { upload } from "../middlewares/multer.js";
 import { verityJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,19 @@ userRoutes.route("/login").post(loginUser);
 // secured route
 userRoutes.route("/logout").post(verityJWT, logoutUser);
 userRoutes.route("/refresh-token").post(refreshAccessToken);
+
+userRoutes.route("/change-password").post(verityJWT, changeCurrentPassword);
+userRoutes.route("/current-user").get(verityJWT, getCurrentUser);
+userRoutes.route("/update-account").patch(verityJWT, updateAccountDetails);
+
+userRoutes
+  .route("/avatar")
+  .patch(verityJWT, upload.single("avatar"), updateUserAvatar);
+userRoutes
+  .route("/cover-image")
+  .patch(verityJWT, upload.single("coverImage"), updateUserCoverImage);
+
+userRoutes.route("/c/:username").get(verityJWT, getUserChannelProfile);
+userRoutes.route("/history").get(verityJWT, getWatchHistory);
 
 export default userRoutes;
