26f84a0c8812218291f9c4aeeebb432e62dc5b75
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-21 16:19:53 +0530

    added comment, playlist, like, tweet models

--- src/models/videoModel.js
+++ src/models/videoModel.js
@@ -42,6 +42,6 @@ const videoSchema = new Schema(
   }
 );
 
-videoSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
