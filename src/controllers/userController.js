a1928c2d6fd6ccc6b978e812d72a4ac09ede8fd1
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-11 06:53:12 +0530

    [bug-fix] - password-changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -238,21 +238,29 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 });
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
-  const { oldPassword, newPassword } = req.body;
+  try {
+    const { oldPassword, newPassword } = req.body;
 
-  const user = await User.findById(req.user?._id);
-  const isPasswordCorrect = await User.isPasswordCorrect(oldPassword);
+    const user = await User.findById(req.user?._id);
+    if (!user) {
+      throw new ApiError(400, "User not found!");
+    }
 
-  if (!isPasswordCorrect) {
-    throw new ApiError(400, "Invalid old password !");
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+      throw new ApiError(400, "Invalid old password !");
+    }
+
+    user.password = newPassword;
+    await user.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Password changed Successfully !"));
+  } catch (error) {
+    throw new ApiError(400, "Password Didn't match!");
   }
-
-  user.password = newPassword;
-  await user.save({ validateBeforeSave: false });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Password changed Successfully !"));
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
