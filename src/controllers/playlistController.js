b48c56bcbb62531c0df942c5be42dd1ddbdf1905
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-15 07:07:33 +0530

    complete create-playlist api

--- src/controllers/playlistController.js
+++ src/controllers/playlistController.js
@@ -1,12 +1,47 @@
 import { Playlist } from "../models/playlistModel.js";
+import { Video } from "../models/videoModel.js";
 import ApiError from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-  const { name, description } = req.body;
+  try {
+    // create playlist.
 
-  //TODO: create playlist
+    // Get name and description from req.body.
+    const { name, description, page = 1, limit = 10 } = req.body;
+
+    // validate name and desciption.
+    if (!name || !description) {
+      throw new ApiError(400, "All Fields are Required!");
+    }
+
+    // Get videos of the User.
+    const userVideos = await Video.find({ owner: req.user._id })
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit));
+
+    //Get videos-ID from userVideos.
+    const videosId = userVideos.map((video) => video._id);
+
+    // creating playlist DB entry
+    const createPlaylist = await Playlist.create({
+      name,
+      description,
+      videos: videosId,
+      owner: req.user._id,
+    });
+
+    // Return the response.
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(201, createPlaylist, "Playlist created successfully!")
+      );
+  } catch (error) {
+    console.error(error);
+    throw new ApiError(500, "Error encountered while creating playlist!");
+  }
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
