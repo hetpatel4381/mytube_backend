651004610661ae01541cbbb1cf92f80da8e05b75
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-15 21:18:35 +0530

    complete get-user-channel-subscribers

--- src/controllers/subscriptionController.js
+++ src/controllers/subscriptionController.js
@@ -40,13 +40,42 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 });
 
 // controller to return subscriber list of a channel
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-  const { channelId } = req.params;
-});
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {});
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-  const { subscriberId } = req.params;
+  try {
+    // Get channel Subscribers.
+
+    // Get channel-Id from req.params and validate it.
+    const { channelId } = req.params;
+    if (!channelId) {
+      throw new ApiError(400, "Channel-ID Required!");
+    }
+
+    // Find all subscriptions for the channel
+    const subscriptions = await Subscription.find({
+      channel: channelId,
+    }).populate("subscriber", "name email");
+
+    if (!subscriptions) {
+      throw new ApiError(400, "Subscriptions Not Found!");
+    }
+
+    // Extract the subscribers from the subscriptions
+    const subscribers = subscriptions.map((sub) => sub.subscriber);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, subscribers, "Subscribers fetched successfully!")
+      );
+  } catch (error) {
+    throw new ApiError(
+      400,
+      "Error Encountered Getting User Channel Subscribers!"
+    );
+  }
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
